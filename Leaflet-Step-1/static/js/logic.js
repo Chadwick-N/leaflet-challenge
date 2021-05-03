90ba4b8d71b9c3710bc4e2898c47943e36a52184
Max Nguyen <chadwick.uxd@gmail.com>
2021-05-03 00:13:28 -0400

    logic update

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -44,18 +44,33 @@ d3.json(earthquakesURL, function(earthquakeData) {
   function chooseColor(depth) {
     switch(true) {
       case depth > 90:
-        return "darkred";
+        return "maroon";
       case depth > 70:
         return "red";
       case depth > 50:
         return "orange";
       case depth > 30:
-        return "yellow";
+        return "gold";
       case depth > 10:
-        return "tan";
+        return "yellow";
       default:
-        return "teal";
+        return "tan";
     }
   };
+  
+  var legend = L.control({position: "bottomright"});
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend"),
+    depth = [-10, 10, 30, 50, 70, 90];
+    
+    div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+  for (var i =0; i < depth.length; i++) {
+    div.innerHTML += 
+    '<i style="background:' + chooseColor(depth[i] + 1) + '"></i> ' +
+        depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+      }
+    return div;
+  };
+  legend.addTo(myMap);
 
 });
